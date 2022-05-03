a70a32097448eac1f1883322c6cc12ce188fadb4
unknown <magma995@gmail.com>
2022-05-03 14:51:44 +0200

    prima bozza di progetto ecommerce

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { PaymentMethodsComponent } from './payment-methods/payment-methods.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:HomeComponent },
+  { path: 'productsList', component:ProductsListComponent },
+  { path: 'productPage', component:ProductPageComponent },
+  { path: 'register', component:RegisterComponent },
+  { path: 'login', component:LoginComponent },
+  { path: 'payment', component:PaymentMethodsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { PaymentMethodsComponent } from './payment-methods/payment-methods.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { ProductPageComponent } from './product-page/product-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+
+    HomeComponent,
+     LoginComponent,
+     RegisterComponent,
+     PaymentMethodsComponent,
+     CartComponent,
+     ProductsListComponent,
+     ProductPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HomeComponent]
 })
 export class AppModule { }
