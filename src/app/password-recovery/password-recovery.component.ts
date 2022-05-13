d105dfdc00ff38d9e733be7771b5a104ba2b1a62
unknown <magma995@gmail.com>
2022-05-13 19:42:47 +0200

    scusa me so sbajato

---
+++ src/app/password-recovery/password-recovery.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-password-recovery',
+  templateUrl: './password-recovery.component.html',
+  styleUrls: ['./password-recovery.component.scss']
+})
+export class PasswordRecoveryComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {}
+  regexMail= /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  
+  subscribeForm = new FormGroup({
+    mail: new FormControl('',[Validators.minLength(1),Validators.required,Validators.pattern(this.regexMail)]),
+    password: new FormControl('',[Validators.required, Validators.minLength(8)])
+  })
+  
+  subscribe(){}
+}
