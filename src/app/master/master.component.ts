cb3f42f0b1880a95474ad12c6d874b544275dd83
Daniel <daniel.damico88@gmail.com>
2022-05-13 14:50:27 +0200

    Aggiunti Box ricerca tramite marchio responsivi e footer responsivo

--- src/app/master/master.component.ts
+++ src/app/master/master.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-master',
@@ -7,11 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MasterComponent implements OnInit {
 
+  regexMail= /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+  subscribeForm = new FormGroup({
+    mail: new FormControl('',[Validators.required,Validators.pattern(this.regexMail)]),
+  })
+  
+
   constructor() {
   
    }
 
   ngOnInit(): void {
+  }
+
+  subscribe(){
+
   }
 
 }
