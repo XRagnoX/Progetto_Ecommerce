d13fab2127f5b514cc1b4b37d842a07bb1fcc657
Daniel <daniel.damico88@gmail.com>
2022-05-14 14:03:09 +0200

    Completata product-page e piccole modifiche al footer

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductPageComponent implements OnInit {
 
-  constructor() { }
+  mainPhoto:string ="assets/img/air-jordan.png"
+  tinyPicOne:string="assets/img/air-jordan.png"
+  tinyPicTwo:string="assets/img/adidas-alpha.png"
+  tinyPicThree:string="assets/img/puma-sneakers.png"
+  tinyPicFour:string="assets/img/vans-sneakers.png"
+
+  selectedColor:string=""
+
+  colorValue1:any = {
+    "colorName":"Nero",
+    "colorCode":"background-color:#222"
+  }
+
+  colorValue2:any = {
+    "colorName":"Blu",
+    "colorCode":"background-color:#6e8cd5"
+  }
+
+  colorValue3:any = {
+    "colorName":"Verde",
+    "colorCode":"background-color:#44c28d"
+  }
+
+  constructor() { 
+
+    this.selectedColor=this.colorValue1.colorName
+  }
 
   ngOnInit(): void {
+  }
+
+  changeMainPhoto(photo:string){
+    this.mainPhoto =photo;
   }
 
 }
